45d95c7c0cf6508e0fdf12e5fe2cf8b3d659c7fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-13 14:42:07 +0200

    feature: display content as table (#13)

--- packages/csv-worker/src/parts/ParseCsv/ParseCsv.ts
+++ packages/csv-worker/src/parts/ParseCsv/ParseCsv.ts
@@ -1,4 +1,5 @@
-import { CsvRow } from '../CsvRow/CsvRow.ts'
+import type { CsvRow } from '../CsvRow/CsvRow.ts'
+import type { ParsedCsv } from '../ParsedCsv/ParsedCsv.ts'
 
 const parseItem = (item: string): string => {
   return item.trim()
@@ -8,8 +9,11 @@ const parseCsvLine = (line: string): CsvRow => {
   return line.split(',').map(parseItem)
 }
 
-export const parseCsv = (content: string): readonly CsvRow[] => {
+export const parseCsv = (content: string): ParsedCsv => {
   const lines = content.split('\n')
   const parsed = lines.map(parseCsvLine)
-  return parsed
+  return {
+    header: parsed[0],
+    content: parsed.slice(1),
+  }
 }
