b4fd45321c503f3d46d3587b88c8194af124c8b0
Le Vivilet <levivlet@pm.me>
2024-10-13 17:16:28 +0200

    unquote

--- packages/csv-worker/src/parts/ParseCsvLine/ParseCsvLine.ts
+++ packages/csv-worker/src/parts/ParseCsvLine/ParseCsvLine.ts
@@ -1,7 +1,14 @@
 import type { CsvRow } from '../CsvRow/CsvRow.ts'
 
+const unquote = (item: string): string => {
+  if (item.startsWith('"') && item.endsWith('"')) {
+    return item.slice(1, -1)
+  }
+  return item
+}
+
 const parseItem = (item: string): string => {
-  return item.trim()
+  return unquote(item).trim()
 }
 
 export const parseCsvLine = (line: string): CsvRow => {
