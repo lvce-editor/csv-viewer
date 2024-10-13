b095629b9db9eeb8e50bc7c3aea3bef31ffa19a3
Le Vivilet <levivlet@pm.me>
2024-10-13 19:52:57 +0200

    position

--- packages/csv-worker/src/parts/GetTextAreaPosition/GetTextAreaPosition.ts
+++ packages/csv-worker/src/parts/GetTextAreaPosition/GetTextAreaPosition.ts
@@ -3,9 +3,17 @@ import type { Position } from '../Position/Position.ts'
 
 export const getTextAreaPosition = (cursor: Cursor): Position => {
   const rowHeight = 20
-  const columnWidth = 80
+  const columnWidth = 120
+  const firstColumnWidth = 40
+  let x = 0
+  if (cursor.columnIndex >= 1) {
+    x += firstColumnWidth
+  }
+  if (cursor.columnIndex >= 2) {
+    x += (cursor.columnIndex - 1) * columnWidth
+  }
   return {
-    x: columnWidth * cursor.columnIndex,
-    y: rowHeight * cursor.rowIndex,
+    x,
+    y: rowHeight * (cursor.rowIndex + 1),
   }
 }
