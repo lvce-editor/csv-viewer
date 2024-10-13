b52d06847fa2bdaf7b79ef1020ada1b74b5e0b72
Le Vivilet <levivlet@pm.me>
2024-10-13 16:15:37 +0200

    feature: use virtual dom for rendering

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -1,43 +1,26 @@
 // TODO use virtual dom in worker
 
-const initialize = (parsed) => {
+const render = (vdom) => {
+  console.log({ vdom })
+  if (vdom.type === 'text') {
+    const node = document.createTextNode(vdom.value)
+    return node
+  }
+  const children = vdom.children || []
+  const $$children = children.map(render)
+  const $Element = document.createElement(vdom.type)
+  if (vdom.className) {
+    $Element.className = vdom.className
+  }
+  $Element.append(...$$children)
+  return $Element
+}
+
+const initialize = (vdom) => {
   const $App = document.createElement('div')
   $App.className = 'App'
-
-  const $Content = document.createElement('div')
-  $Content.className = 'Content'
-
-  const $Table = document.createElement('table')
-  $Table.className = 'Table'
-
-  const $Tbody = document.createElement('tbody')
-  $Tbody.className = 'TableBody'
-
-  const { content, header } = parsed
-
-  const $Thead = document.createElement('thead')
-  $Thead.className = 'TableHead'
-  for (const element of header) {
-    const $Th = document.createElement('th')
-    $Th.textContent = element
-    $Thead.append($Th)
-  }
-
-  for (const row of content) {
-    const $Row = document.createElement('tr')
-    $Row.className = 'Row'
-    for (const element of row) {
-      const $Cell = document.createElement('td')
-      $Cell.className = 'Cell'
-      $Cell.textContent = element
-      $Row.append($Cell)
-    }
-    $Tbody.append($Row)
-  }
-  $Table.append($Thead, $Tbody)
-
-  $Content.append($Table)
-  $App.append($Content)
+  const $Rendered = render(vdom)
+  $App.append($Rendered)
   document.body.append($App)
 }
 
