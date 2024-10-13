320375419864515ca94da3fd8ecc163f1dba7a82
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-13 14:06:39 +0200

    feature: display csv content (#5)

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -1,1 +1,15 @@
-console.log('hello from indexjs')
+// TODO use virtual dom in worker
+
+const initialize = (textContent) => {
+  const app = document.createElement('div')
+  app.className = 'App'
+  const content = document.createElement('div')
+  content.className = 'Content'
+  content.textContent = textContent
+  app.append(content)
+  document.body.append(app)
+}
+
+const rpc = globalThis.lvceRpc({
+  initialize,
+})
