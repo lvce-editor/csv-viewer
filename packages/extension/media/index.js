34e4d13aebba0fb2974574dbdc6a78e4574e13c2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-13 14:30:44 +0200

    feature: improve content display (#11)

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -1,13 +1,25 @@
 // TODO use virtual dom in worker
 
 const initialize = (parsed) => {
-  const app = document.createElement('div')
-  app.className = 'App'
-  const content = document.createElement('div')
-  content.className = 'Content'
-  content.textContent = JSON.stringify(parsed, null, 2)
-  app.append(content)
-  document.body.append(app)
+  const $App = document.createElement('div')
+  $App.className = 'App'
+  const $Content = document.createElement('div')
+  $Content.className = 'Content'
+
+  const rows = parsed
+  for (const row of rows) {
+    const $Row = document.createElement('div')
+    $Row.className = 'Row'
+    for (const element of row) {
+      const $Cell = document.createElement('div')
+      $Cell.className = 'Cell'
+      $Cell.textContent = element
+      $Row.append($Cell)
+    }
+    $Content.append($Row)
+  }
+  $App.append($Content)
+  document.body.append($App)
 }
 
 const rpc = globalThis.lvceRpc({
