3e14735471a934c9030067c17032bcc33bd8ee95
Le Vivilet <levivlet@pm.me>
2024-10-13 17:35:07 +0200

    feature: send parsed row and column index to worker

--- packages/csv-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/csv-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,10 @@
 import * as GetCsvVirtualDom from '../GetCsvVirtualDom/GetCsvVirtualDom.ts'
 import * as ParseCsv from '../ParseCsv/ParseCsv.ts'
+import * as WebView from '../WebView/WebView.ts'
 
 export const commandMap = {
   'Csv.parse': ParseCsv.parseCsv,
   'Csv.getVirtualDom': GetCsvVirtualDom.getCsvVirtualDom,
+  'WebView.create': WebView.create,
+  'WebView.setCursor': WebView.setCursor,
 }
---
+++ packages/csv-worker/src/parts/WebView/WebView.ts
@@ -0,0 +1,20 @@
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+export const create = (id: number) => {
+  const webview = {
+    rowIndex: 0,
+    columnIndex: 0,
+  }
+  WebViewStates.set(id, webview)
+}
+
+export const setCursor = (id: number, rowIndex: number, columnIndex: number) => {
+  const webView = WebViewStates.get(id)
+  const newWebView = {
+    ...webView,
+    rowIndex,
+    columnIndex,
+  }
+  WebViewStates.set(id, newWebView)
+  console.log({ newWebView })
+}
