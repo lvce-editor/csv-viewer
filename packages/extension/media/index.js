df563bc6542b2ee26f0a8f082bf1ad86660e43d6
Le Vivilet <levivlet@pm.me>
2024-10-13 14:37:35 +0200

    feature: display content as table

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -3,21 +3,40 @@
 const initialize = (parsed) => {
   const $App = document.createElement('div')
   $App.className = 'App'
+
   const $Content = document.createElement('div')
   $Content.className = 'Content'
 
-  const rows = parsed
-  for (const row of rows) {
-    const $Row = document.createElement('div')
+  const $Table = document.createElement('table')
+  $Table.className = 'Table'
+
+  const $Tbody = document.createElement('tbody')
+  $Tbody.className = 'TableBody'
+
+  const { content, header } = parsed
+
+  const $Thead = document.createElement('thead')
+  $Thead.className = 'TableHead'
+  for (const element of header) {
+    const $Th = document.createElement('th')
+    $Th.textContent = element
+    $Thead.append($Th)
+  }
+
+  for (const row of content) {
+    const $Row = document.createElement('tr')
     $Row.className = 'Row'
     for (const element of row) {
-      const $Cell = document.createElement('div')
+      const $Cell = document.createElement('td')
       $Cell.className = 'Cell'
       $Cell.textContent = element
       $Row.append($Cell)
     }
-    $Content.append($Row)
+    $Tbody.append($Row)
   }
+  $Table.append($Thead, $Tbody)
+
+  $Content.append($Table)
   $App.append($Content)
   document.body.append($App)
 }
