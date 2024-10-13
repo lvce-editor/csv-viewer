015eeb1e87f521983be78bee170964e9c17c0dc6
Le Vivilet <levivlet@pm.me>
2024-10-13 11:47:12 +0200

    feature: add initial files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# csv-viewer
-CSV Viewer
+# csb-viewer
+
+## Contributing
+
+```sh
+git clone git@github.com:lvce-editor/csv-viewer.git &&
+cd csv-viewer &&
+npm ci &&
+npm test
+```
+
+## Gitpod
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/lvce-editor/csv-viewer)
---
+++ packages/extension/src/parts/Main/Main.ts
@@ -0,0 +1,36 @@
+import * as FilterAggregates from '../FilterAggregates/FilterAggregates.ts'
+
+const webViewProvider = {
+  id: 'builtin.csv-viewer',
+  async create(webView, uri) {
+    // TODO ask csv worker to create virtual dom
+    // TODO support connecting state to webview
+    // @ts-ignore
+    this.aggregates = aggregrates
+    // @ts-ignore
+    this.webView = webView
+  },
+  async open(uri, webView) {
+    // const content = await vscode.readFile(uri)
+    // webView.postMessage({
+    //   jsonrpc: '2.0',`
+    //   method: 'setContent',
+    //   params: [content],
+    // })
+  },
+  commands: {
+    async handleInput(text) {
+      // @ts-ignore
+      const aggregates = webViewProvider.aggregates
+      // @ts-ignore
+      const webView = webViewProvider.webView
+      const filtered = FilterAggregates.filterAggregates(aggregates, text)
+      await webView.invoke('setContent', filtered)
+    },
+  },
+}
+
+export const activate = () => {
+  // @ts-ignore
+  vscode.registerWebViewProvider(webViewProvider)
+}
