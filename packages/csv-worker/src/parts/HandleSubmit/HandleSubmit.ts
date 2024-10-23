9c7af02a7df21f0ef7c7fb1e109ef25e15beaa23
Le Vivilet <levivlet@pm.me>
2024-10-23 21:19:34 +0200

    feature: implement edit cell functionality

--- packages/csv-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/csv-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -3,9 +3,14 @@ import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handleSubmit = (id: number): void => {
   const state = WebViewStates.get(id)
+  const { rowIndex, columnIndex, cells, value } = state
+  const oldRow = cells[rowIndex]
+  const newRow = [...oldRow.slice(0, columnIndex), value, ...oldRow.slice(columnIndex + 1)]
+  const newCells = [...cells.slice(0, rowIndex), newRow, ...cells.slice(rowIndex + 1)]
   const newState: WebView = {
     ...state,
     textArea: false,
+    cells: newCells,
   }
   WebViewStates.set(id, newState)
 }
