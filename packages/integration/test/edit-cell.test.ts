597806af9aa3866bb8e500cd204a1a936811f4de
Le Vivilet <levivlet@pm.me>
2024-10-23 20:57:36 +0200

    feature: add edit cell test

--- packages/integration/test/edit-cell.test.ts
+++ packages/integration/test/edit-cell.test.ts
@@ -1,17 +1,17 @@
 import { testWorker } from '../src/testWorker.ts'
 import { test, expect } from '@jest/globals'
 
-test('focus column right', async () => {
+test('focus', async () => {
   const execMap = {}
   const worker = await testWorker({
     execMap,
   })
   const id = 1
   await worker.execute('WebView.create', id)
-  await worker.execute('WebView.setCursor', id, 0, 0)
-  await worker.execute('WebView.handleKeyDown', id, 'ArrowRight')
+  await worker.execute('WebView.setCursor', id, 0, 1)
+  await worker.execute('WebView.handleKeyDown', id, 'ArrowLeft')
   expect(await worker.execute('WebView.getPosition', id)).toEqual({
     rowIndex: 0,
-    columnIndex: 1,
+    columnIndex: 0,
   })
 })
