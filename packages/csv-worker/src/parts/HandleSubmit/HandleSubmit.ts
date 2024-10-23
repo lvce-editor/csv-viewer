d9f7e076b01377637e1669a9cca3c342d5bd582c
Le Vivilet <levivlet@pm.me>
2024-10-23 21:45:32 +0200

    fix: off by one error when editing cells

--- packages/csv-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/csv-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -5,12 +5,13 @@ export const handleSubmit = (id: number): void => {
   const state = WebViewStates.get(id)
   const { rowIndex, columnIndex, cells, value } = state
   const oldRow = cells[rowIndex]
-  const newRow = [...oldRow.slice(0, columnIndex), value, ...oldRow.slice(columnIndex + 1)]
+  const newRow = [...oldRow.slice(0, columnIndex - 1), value, ...oldRow.slice(columnIndex + 1)]
   const newCells = [...cells.slice(0, rowIndex), newRow, ...cells.slice(rowIndex + 1)]
   const newState: WebView = {
     ...state,
     textArea: false,
     cells: newCells,
   }
+  console.log({ newCells })
   WebViewStates.set(id, newState)
 }
