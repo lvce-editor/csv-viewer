d0664674ea7ca5c5aee91d8b32586bacc20f836a
Le Vivilet <levivlet@pm.me>
2024-10-23 20:59:34 +0200

    add test

--- packages/integration/test/edit-cell.test.ts
+++ packages/integration/test/edit-cell.test.ts
@@ -1,17 +1,17 @@
 import { testWorker } from '../src/testWorker.ts'
 import { test, expect } from '@jest/globals'
 
-test('focus', async () => {
+test.skip('edit cell', async () => {
   const execMap = {}
   const worker = await testWorker({
     execMap,
   })
   const id = 1
   await worker.execute('WebView.create', id)
-  await worker.execute('WebView.setCursor', id, 0, 1)
-  await worker.execute('WebView.handleKeyDown', id, 'ArrowLeft')
-  expect(await worker.execute('WebView.getPosition', id)).toEqual({
-    rowIndex: 0,
-    columnIndex: 0,
-  })
+  await worker.execute('WebView.setCells', id, [['a']])
+  await worker.execute('WebView.setCursor', id, 0, 0)
+  await worker.execute('WebView.setTextArea', id, true)
+  await worker.execute('WebView.handleInput', id, 'b')
+  await worker.execute('WebView.handleSubmit', id)
+  expect(await worker.execute('WebView.getCells', id)).toEqual([['b']])
 })
