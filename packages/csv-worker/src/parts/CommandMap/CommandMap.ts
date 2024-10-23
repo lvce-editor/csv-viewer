c151c34d19177ac980009ab474859f283868b3e8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-23 20:46:57 +0200

    feature: add focus integration test (#41)

--- packages/csv-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/csv-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,13 +1,15 @@
 import * as GetCsvVirtualDom from '../GetCsvVirtualDom/GetCsvVirtualDom.ts'
+import * as GetPosition from '../GetPosition/GetPosition.ts'
 import * as ParseCsv from '../ParseCsv/ParseCsv.ts'
 import * as WebView from '../WebView/WebView.ts'
 
 export const commandMap = {
-  'Csv.parse': ParseCsv.parseCsv,
   'Csv.getVirtualDom': GetCsvVirtualDom.getCsvVirtualDom,
+  'Csv.parse': ParseCsv.parseCsv,
   'WebView.create': WebView.create,
-  'WebView.setCursor': WebView.setCursor,
   'WebView.getCursor': WebView.getCursor,
+  'WebView.getPosition': GetPosition.getPosition,
   'WebView.handleKeyDown': WebView.handleKeyDown,
+  'WebView.setCursor': WebView.setCursor,
   'WebView.setTextarea': WebView.setTextArea,
 }
