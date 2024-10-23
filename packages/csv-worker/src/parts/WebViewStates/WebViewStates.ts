5da010c35206f9b97737e6e50a612bce5354eafc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-23 21:08:23 +0200

    feature: add webView interface (#46)

--- packages/csv-worker/src/parts/WebViewStates/WebViewStates.ts
+++ packages/csv-worker/src/parts/WebViewStates/WebViewStates.ts
@@ -1,9 +1,11 @@
+import type { WebView } from '../WebView/WebView.ts'
+
 const webviews = Object.create(null)
 
-export const set = (id: number, webView: any) => {
+export const set = (id: number, webView: WebView): void => {
   webviews[id] = webView
 }
 
-export const get = (id: number) => {
+export const get = (id: number): WebView => {
   return webviews[id]
 }
