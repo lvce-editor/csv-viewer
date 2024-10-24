3f9fbf4fe9c7e47762094c162de513a4b7c3caa3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-24 19:58:14 +0200

    feature: add saveState command (#56)

--- packages/csv-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/csv-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,15 +2,16 @@ import * as Create from '../Create/Create.ts'
 import * as GetCells from '../GetCells/GetCells.ts'
 import * as GetCursor from '../GetCursor/GetCursor.ts'
 import * as GetPosition from '../GetPosition/GetPosition.ts'
+import * as GetTextArea from '../GetTextArea/GetTextArea.ts'
 import * as GetVirtualDom from '../GetVirtualDom/GetVirtualDom.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as HandleKeyDown from '../HandleKeyDown/HandleKeyDown.ts'
 import * as HandleSubmit from '../HandleSubmit/HandleSubmit.ts'
 import * as ParseCsv from '../ParseCsv/ParseCsv.ts'
+import * as SaveState from '../SaveState/SaveState.ts'
 import * as SetCells from '../SetCells/SetCells.ts'
 import * as SetCursor from '../SetCursor/SetCursor.ts'
 import * as SetHeader from '../SetHeader/SetHeader.ts'
-import * as GetTextArea from '../GetTextArea/GetTextArea.ts'
 import * as SetTextArea from '../SetTextArea/SetTextArea.ts'
 
 export const commandMap = {
@@ -27,6 +28,7 @@ export const commandMap = {
   'WebView.handleInput': HandleInput.handleInput,
   'WebView.handleKeyDown': HandleKeyDown.handleKeyDown,
   'WebView.handleSubmit': HandleSubmit.handleSubmit,
+  'WebView.saveState': SaveState.saveState,
   'WebView.setCells': SetCells.setCells,
   'WebView.setCursor': SetCursor.setCursor,
   'WebView.setHeader': SetHeader.setHeader,
