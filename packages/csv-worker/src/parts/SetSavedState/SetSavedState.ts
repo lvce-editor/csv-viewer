36016fa28123985265a6fffeb5c599fb39293084
Le Vivilet <levivlet@pm.me>
2024-10-25 22:13:08 +0200

    feature; save and restore webview state

---
+++ packages/csv-worker/src/parts/SetSavedState/SetSavedState.ts
@@ -0,0 +1,37 @@
+import { WebView } from '../WebView/WebView.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+const getSavedRowIndex = (savedState: any): number => {
+  if (savedState && typeof savedState.rowIndex === 'number') {
+    return savedState.rowIndex
+  }
+  return 0
+}
+
+const getSavedColumnIndex = (savedState: any) => {
+  if (savedState && typeof savedState.columnIndex === 'number') {
+    return savedState.columnIndex
+  }
+  return 0
+}
+
+const getSavedValue = (savedState: any) => {
+  if (savedState && typeof savedState.value === 'string') {
+    return savedState.value
+  }
+  return ''
+}
+
+export const setSavedState = (id: number, savedState: any): void => {
+  const webView = WebViewStates.get(id)
+  const rowIndex = getSavedRowIndex(savedState)
+  const columnIndex = getSavedColumnIndex(savedState)
+  const value = getSavedValue(savedState)
+  const newState: WebView = {
+    ...webView,
+    rowIndex,
+    columnIndex,
+    value,
+  }
+  WebViewStates.set(id, newState)
+}
