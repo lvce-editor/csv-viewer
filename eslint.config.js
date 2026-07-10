8113a166a8903f720fe358fa0805b38749f4a07f
Le Vivilet <levivilet@pm.me>
2026-07-10 09:59:17 +0000

    feature: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,21 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  {
+    files: ['packages/**/*.ts'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    },
+  },
+  {
+    files: ['packages/integration/**/*.ts'],
+    rules: {
+      'jest/no-disabled-tests': 'off',
+      'unicorn/no-global-object-property-assignment': 'off',
+    },
+  },
+]
